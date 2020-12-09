444e822bc4b5c43a840e37a49158fc80ee74e8bb
josephchang98 <Joseph.H.Chang-1@ou.edu>
2020-12-08 22:13:23 -0600

    input code (untested)

--- robot_ws/src/robot_control/src/input_node.py
+++ robot_ws/src/robot_control/src/input_node.py
@@ -14,6 +14,8 @@ class InputReader:
     def __init__(self):
         #self.tasks_pub = rospy.Publisher('/robot/tasks', tasks, queue_size = 10)
         self.choice_pub = rospy.Publisher('/robot/choice', Int32, queue_size = 10)
+        self.tour_pub = rospy.Publisher('/robot/tour', Int32, queue_size = 10)
+        self.dest_pub = rospy.Publisher('/robot/destination', Int32, queue_size = 10)
         self.busy_sub = rospy.Subscriber('/robot/busy_bool', Bool, self.input_callback)
     
     # Callback function for actually getting coordinate input
@@ -22,10 +24,12 @@ class InputReader:
         #myTasks = tasks()
 
         myChoice = Int32()
+        tour = Int32()
+        dest = Int32()
 
         if data.data == False:
             rospy.sleep(1)
-            print "To start a tour enter an option number to choose a location to start at and press Enter \n" + "1) Start at the West entrance to Devon \n" + "2) Start at the East entrance to Devon \n"
+            print "To start the service enter an option number to choose a location to start at and press Enter \n" + "1) Start at the West entrance to Devon \n" + "2) Start at the East entrance to Devon \n"
 
             string_input = raw_input()
             while string_input != "":
@@ -53,6 +57,33 @@ class InputReader:
                 string_input = raw_input()
 
             self.choice_pub.publish(myChoice)
+            
+            print "Select which service you would like to receive \n" + "1) Tour \n" + "2) Destination guidance \n"
+            
+            string_input = raw_input()
+            while string_input != "":
+                
+                if string_input == "1":
+                    tour.data = 1
+                elif string_input == "2":
+                    tour.data = 0
+                    print "Select which destination you would like to go to \n" + "1) Rawl Engineering Facility \n" + "Rawl Practice Bay \n" + "3) Devon Energy Hall \n" + "4) Devon Computer Lab \n" + "5) Devon Electrical Lab \n"
+                    
+                    string_input = raw_input()
+                    while string_input != "":
+                        if string_input == "1":
+                            dest.data = 1
+                        elif string_input == "2": 
+                            dest.data = 2
+                        elif string_input == "3": 
+                            dest.data = 2
+                        elif string_input == "4": 
+                            dest.data = 2
+                        elif string_input == "5": 
+                            dest.data = 2
+            
+            self.tour_pub.publish(tour)
+            self.dest_pub.publish(dest)
             rospy.sleep(5)
 
 # Initializes our node as 'input_node'
